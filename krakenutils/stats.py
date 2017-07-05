a29cd7d8d7115d301a2be00f7ceb1aa5e2d3e1f5
tms1337 <faruk1337@gmail.com>
2017-07-05 18:09:04 +0200

    extract ProviderBase for kraken providers

--- krakenutils/stats.py
+++ krakenutils/stats.py
@@ -1,15 +1,10 @@
 import krakenex
+from .providerbase import ProviderBase
 
 
-class StatsProvider:
+class StatsProvider(ProviderBase):
     def __init__(self, currency="USD", crypto="ETH", api=krakenex.API()):
-        self._check_currency(currency)
-        self._check_crypto(crypto)
-
-        self.currency = currency
-        self.crypto = crypto
-
-        self.k = api
+        super(StatsProvider, self).__init__(currency, crypto, api)
 
     def average_price(self, period_seconds=None):
         prices = self.price_history(period_seconds)
@@ -48,22 +43,4 @@ class StatsProvider:
 
         return prices[-1]
 
-    def _check_currency(self, currency):
-        currencies = ["USD", "EUR"]
-        if not currency in currencies:
-            raise ValueError("Currency must be one of %s", str(currencies))
 
-    def _check_crypto(self, crypto):
-        cryptos = ["BTC", "ETH", "XRP"]
-        if not crypto in cryptos:
-            raise ValueError("Cryptocurrency must be one of %s", str(cryptos))
-
-    def _form_pair(self):
-        return "X%sZ%s" % (self.crypto, self.currency)
-
-    def _check_response(self, server_response):
-        if "error" not in server_response:
-            raise ValueError("Server responded with invalid response")
-
-        if len(server_response["error"]) != 0:
-            raise RuntimeError("Server responded with error")
--- krakenutils/trade.py
+++ krakenutils/trade.py
@@ -1,3 +1,9 @@
-class TradeProvider:
+from .providerbase import ProviderBase
+
+
+class TradeProvider(ProviderBase):
     def __init__(self, api_key):
+        pass
+
+    def create_offer(self):
         pass
