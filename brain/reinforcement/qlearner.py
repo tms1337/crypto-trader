4db78aba4b1348fde0f9eea15a7aaaba9cf31f29
tms1337 <faruk1337@gmail.com>
2017-07-25 15:22:23 +0200

    add state vector

---
+++ brain/reinforcement/qlearner.py
@@ -0,0 +1,2 @@
+class QLearner:
+    pass
---
+++ brain/reinforcement/statevector.py
@@ -0,0 +1,28 @@
+from bot.util.asserting import TypeChecker
+
+
+class StateVector:
+    def __init__(self, dimension_n, initial_value=None):
+        TypeChecker.check_type(dimension_n, int)
+        self.dimension_n = dimension_n
+
+        TypeChecker.check_one_of_types(initial_value, [tuple, list])
+        for v in initial_value:
+            TypeChecker.check_one_of_types(v, [float, int])
+
+        assert len(initial_value) == dimension_n, \
+            "Initial value must be vector of length %d" % dimension_n
+        self.value = tuple(initial_value)
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        TypeChecker.check_one_of_types(value, [tuple, list])
+        for v in value:
+            TypeChecker.check_one_of_types(v, [float, int])
+
+        assert len(value) == self.dimension_n, \
+            "Value must be vector of length %d" % self.dimension_n
+
+        self.value = tuple(value)
