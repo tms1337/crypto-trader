5ea4539aab7d7590d7d70520431cbf77d413078d
tms1337 <faruk1337@gmail.com>
2017-08-07 02:14:45 +0200

    add keras and tensorflow to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,8 @@ sudo apt-get update
 sudo apt-get install virtualenv -y
 sudo apt-get install python3-pip -y
 
+pip3 install --upgrade pip
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get update
@@ -23,6 +25,11 @@ pip3 install nose==1.3.7
 pip3 install simplejson==3.11.1
 pip3 install cachetools==2.0.0
 pip3 install pymongo==3.4.0
+pip3 install tensorflow=1.2.1
+pip3 install Keras==2.0.6
+pip3 install tensorflow==1.2.1
+pip3 install pandas=0.20.3
+
 pip3 install git+https://github.com/tms1337/python3-krakenex.git
 pip3 install git+https://github.com/s4w3d0ff/python-poloniex
 pip3 install git+https://github.com/ericsomdahl/python-bittrex.git
