b1999c7348f6fefbef782ad17b841c3c711e09c8
tms1337 <faruk1337@gmail.com>
2017-08-14 17:05:27 +0200

    wip

--- testing.py
+++ testing.py
@@ -9,14 +9,21 @@ from sklearn.model_selection import KFold
 import numpy as np
 import sys
 
+
 def load_data(data, seq_len, test_perc):
+    # for i in range(1, len(data)):
+    #     for j in range(5):
+    #         data[i][j] = (data[i][j] - data[i - 1][j]) / data[i - 1][j]
+    #
+    # data = data[1:]
+
     sequence_length = seq_len + 1
     x = []
-    y= []
+    y = []
     for i in range(len(data) - sequence_length - 1):
         x.append(data[i: i + sequence_length])
         # will it increase
-        if data[i + sequence_length] > data[i + sequence_length - 1]:
+        if data[i + sequence_length][1] > data[i + sequence_length - 1][3]:
             y.append([0, 1])
         else:
             y.append([1, 0])
@@ -24,53 +31,69 @@ def load_data(data, seq_len, test_perc):
     x = np.array(x)
     y = np.array(y)
 
-    print(x, y)
+    divisible_n = 100 * int(x.shape[0] / 100)
+    x = x[:divisible_n]
+    y = y[:divisible_n]
 
     test_n = int(test_perc * x.shape[0])
-    x_train, x_test = x[:-test_n], x[test_n:]
-    y_train, y_test = y[:-test_n], y[test_n:]
+    x_train, x_test = x[:-test_n], x[-test_n:]
+    y_train, y_test = y[:-test_n], y[-test_n:]
 
     return [x_train, y_train, x_test, y_test]
 
-df = pd.read_csv(header=None,
-                 usecols=[1],
-                 filepath_or_buffer="/data/krakenUSD.csv",
-                 nrows=int(2e6))
+
+df = pd.read_csv(sep=" ",
+                 filepath_or_buffer="/data/full_daily_ohlc.csv.reversed")
+
+data = df.values
 
 print("Data loaded")
-scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 
-data = scaler.fit_transform(df.values)
-
-seq_len = 50
+seq_len = 5
 test_perc = 0.3
+
+epochs = 1
+batch_size = 10
 
 x_train, y_train, x_test, y_test = load_data(data, seq_len, test_perc)
 
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
 
 predictor = Sequential()
 predictor.add(LSTM(activation="linear",
-                   input_dim=1,
-                   output_dim=50,
+                   batch_input_shape=(batch_size, x_train.shape[1], x_train.shape[2]),
+                   stateful=False,
+                   units=100,
                    return_sequences=True,
-                   dropout=0.2))
-predictor.add(LSTM(100, activation="linear", return_sequences=False, dropout=0.2))
+                   dropout=0.5,
+                   recurrent_dropout=0.5))
+predictor.add(LSTM(activation="linear",
+                   return_sequences=False,
+                   units=100))
+predictor.add(Dense(200, activation="linear"))
+predictor.add(Dropout(0.5))
+predictor.add(Dense(100, activation="linear"))
+predictor.add(Dropout(0.5))
 predictor.add(Dense(20, activation="linear"))
-predictor.add(Dropout(0.4))
+predictor.add(Dropout(0.5))
 predictor.add(Dense(2, activation="softmax"))
 
-epochs = 5
-batch_size = 100
-
-predictor.compile(optimizer=Adam(lr=0.006),
+predictor.compile(optimizer=Adam(lr=0.00001),
                   loss=categorical_crossentropy,
                   metrics=["accuracy"])
 
 predictor.fit(x_train,
               y_train,
+              shuffle=True,
               epochs=epochs,
               batch_size=batch_size)
 
-predictor.evaluate(x_test,
-                   y_test)
+acc = predictor.evaluate(x_test,
+                         y_test,
+                         batch_size=batch_size)
 
+print(acc)
+print(x_test[-batch_size:])
+print(predictor.predict_proba(x_test[-batch_size:], batch_size=batch_size))
+print(predictor.predict_on_batch(x_test[-batch_size:]))
